29d836bbb0d7c9dd403d48beb4494e63fbf280ad
jiakun8 <jrjiakun@jd.com>
2019-05-15 13:58:37 +0800

    nothing

--- src/main/java/com/kun/practise/natives/PlainNioServer.java
+++ src/main/java/com/kun/practise/natives/PlainNioServer.java
@@ -13,27 +13,48 @@ import java.util.Set;
 
 /**
  * Created by jrjiakun on 2018/9/13
+ *
+ * NIO利用单线程轮询事件的机制高效的定位就绪的Channel来决定做什么。仅仅select阶段是阻塞的，应用的扩展性非常高
+ *
+ *
+ * NIO组成部分
+ *Buffer
+ *Channel
+ *Selector
+ *Charset
  */
 public class PlainNioServer {
     public void serve(int port) throws IOException {
+
+        // Selector.open创建一个Selector(调度员角色)
+        Selector selector = Selector.open();
+
+
+        // serverChannel
         ServerSocketChannel serverChannel = ServerSocketChannel.open();
         serverChannel.configureBlocking(false);
-
-
+        //绑定端口
         ServerSocket ss = serverChannel.socket();
         InetSocketAddress address = new InetSocketAddress(port);
         ss.bind(address);
-        Selector selector = Selector.open();
+
+
+        //注册到Selector
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+
         final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
         for (;;){
             try {
+                //阻塞等待就绪的 Channel，这是关键点之一
                 selector.select();
             } catch (IOException ex) {
                 ex.printStackTrace();
                 //handle exception
                 break;
             }
+
+
             Set<SelectionKey> readyKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = readyKeys.iterator();
             while (iterator.hasNext()) {
